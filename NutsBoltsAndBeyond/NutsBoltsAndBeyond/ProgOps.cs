3639babd1667dc939fc66a26563d0d27ce6cf180
haywood-d-johnson <haywood.d.johnson@gmail.com>
2021-02-26 05:07:24 -0600

    init: proj init w/ DB connect

---
+++ NutsBoltsAndBeyond/NutsBoltsAndBeyond/ProgOps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NutsBoltsAndBeyond
+{
+    class ProgOps
+    {
+        #region Database Open/Close
+
+        public static SqlConnection _dbConnect;
+
+        public static void ConnectDB()
+        {
+            _dbConnect = new SqlConnection(Utils.CONNECT_STRING);
+
+            try
+            {
+                if (_dbConnect.State == ConnectionState.Closed) _dbConnect.Open();
+                // For test only
+                MessageBox.Show("Connection Successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "There was a problem", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        public static void CloseDB()
+        {
+            _dbConnect.Close();
+            _dbConnect.Dispose();
+        }
+
+        #endregion
+
+    }
+}
---
+++ NutsBoltsAndBeyond/NutsBoltsAndBeyond/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutsBoltsAndBeyond
+{
+    class Utils
+    {
+        /*
+         *  Helper functions & global variables
+        */
+
+        #region Global Variables
+
+        public static String CONNECT_STRING = @"Server=cstnt.tstc.edu;Database=inew2330sp21;User Id=group1sp212330;password=1597159";
+
+        #endregion
+
+        #region id generator
+        /*
+         *  Creates random 7-digit ID
+         *  @params: void
+         *  @return: int
+         */
+
+        public static int _IDGenerator()
+        {
+            var random = new Random();
+            String id = String.Empty;
+            for (int i = 0; i < 7; i++)
+                id = String.Concat(id, random.Next(10).ToString());
+
+            return Int32.Parse(id);
+        }
+
+        #endregion 
+    }
+}
